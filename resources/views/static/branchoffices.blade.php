7ef72b7870b90c304f12450639b4bf565f510c36
Sebastian Lena <sebas.strogg@gmail.com>
2019-11-24 01:06:40 -0300

    Fix some links

--- resources/views/static/branchoffices.blade.php
+++ resources/views/static/branchoffices.blade.php
@@ -7,7 +7,7 @@
         <nav aria-label="breadcrumb" class="breadcrumb-nav">
             <div class="container">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Sucursales</li>
                 </ol>
             </div><!-- End .container -->
