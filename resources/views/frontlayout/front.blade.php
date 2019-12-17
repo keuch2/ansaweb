98dffbe97eb7f325469a516beab30cf12c3bd73d
Sebastian Lena <sebas.strogg@gmail.com>
2019-12-17 11:50:11 -0300

    Design updates

--- resources/views/frontlayout/front.blade.php
+++ resources/views/frontlayout/front.blade.php
@@ -105,5 +105,89 @@
 </script>
 
 
+
+<footer class="footer">
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="widget">
+                                <h4 class="widget-title">Neumáticos</h4>
+
+                                <ul class="links">
+
+                                    @foreach($vehicleTypes as $vehicleType)
+                                        <li><a href="{{route('tires-list-byVehicleType', ['vehicleTypeId' => $vehicleType->id])}}">{{$vehicleType->vehicle_type}}</a></li>
+                                    @endforeach
+
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/1">Autos</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/2">Camionetas</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/3">Camiones</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/4">Motos</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/6">Agricolas</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/7">Utilitarios</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/tires/byVehicleType/8">OTR/Industriales</a></li>--}}
+                                </ul>
+                            </div><!-- End .widget -->
+                        </div><!-- End .col-md-3 -->
+
+                        <div class="col-md-4">
+                            <div class="widget">
+                                <h4 class="widget-title">Productos</h4>
+
+                                <ul class="links">
+
+                                    @foreach($productCategories as $productCategory)
+                                        <li><a href="{{route('products-list-byCategoryId', ['productCategoryId' => $productCategory->id])}}">{{$productCategory->category_name}}</a></li>
+                                    @endforeach
+
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/products/byCategoryId/2">Cámaras</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/products/byCategoryId/3">Llantas</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/products/byCategoryId/4">Tratamiento Multifunciones para el Diesel</a></li>--}}
+{{--                                    <li><a href="http://www.americaneumaticos.com.py/products/byCategoryId/5">Blindaje de  cubiertas contra pinchazos</a></li>--}}
+                                </ul>
+                            </div><!-- End .widget -->
+                        </div><!-- End .col-md-3 -->
+
+                        <div class="col-md-4">
+                            <div class="widget">
+
+                                <h4 class="widget-title">Sobre Nosotros</h4>
+
+                                <ul class="links">
+                                    <li><a href="{{route('home')}}">Inicio</a></li>
+                                    <li><a href="{{route('static-services')}}">Servicios</a></li>
+                                    <li><a href="{{route('static-aboutus')}}">Acerca de ANSA</a></li>
+                                    <li><a href="{{route('static-branchoffices')}}">Sucursales</a></li>
+                                </ul>
+                            </div><!-- End .widget -->
+                        </div><!-- End .col-md-3 -->
+
+
+                    </div><!-- End .row -->
+                </div><!-- End .col-lg-8 -->
+
+
+            </div><!-- End .row -->
+        </div><!-- End .container -->
+    </div><!-- End .footer-middle -->
+
+    <div class="container">
+        <div class="footer-bottom">
+            <p class="footer-copyright">América Neumáticos S.A. &copy;  2019.  All Rights Reserved</p>
+
+
+            <div class="social-icons">
+                <a href="https://www.facebook.com/americaneumaticospy/" class="social-icon" target="_blank"><i class="icon-facebook"></i></a>
+                <a href="https://www.instagram.com/americaneumaticospy/" class="social-icon" target="_blank"><i class="icon-instagram"></i></a>
+            </div><!-- End .social-icons -->
+        </div><!-- End .footer-bottom -->
+    </div><!-- End .containr -->
+</footer><!-- End .footer -->
+
+
+
 </body>
 </html>
